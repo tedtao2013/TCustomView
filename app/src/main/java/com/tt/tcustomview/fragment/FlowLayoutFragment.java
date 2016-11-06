329933bf1395571490ab3a51a64db10a4352c702
tedtao2013 <1804039122@qq.com>
2016-11-06 22:19:45 +0800

    add FlowLayoutFragment

---
+++ app/src/main/java/com/tt/tcustomview/fragment/FlowLayoutFragment.java
@@ -0,0 +1,23 @@
+package com.tt.tcustomview.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * FileName:
+ * com.tt.tcustomview.fragment.FlowLayoutFragment.java
+ * Author: TT
+ * Date: 2016-11-06
+ * Description: <描述当前版本功能>
+ */
+public class FlowLayoutFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
